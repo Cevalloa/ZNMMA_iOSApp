9da2d48c2997d8a3bfd2988015e4c56e2ad91a03
Cevalloa <alexcevallos@alexcevllossmbp.home>
2014-06-01 14:33:35 -0400

    Initial Commit

---
+++ Zealous Nation MMA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Zealous Nation MMA
+//
+//  Created by Alex Cevallos on 6/1/14.
+//  Copyright (c) 2014 AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Zealous Nation MMA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Zealous Nation MMA
+//
+//  Created by Alex Cevallos on 6/1/14.
+//  Copyright (c) 2014 AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
